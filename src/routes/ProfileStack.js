2db3fdb22f760beed6fc24562707c15acb274380
Elisa <elisangli11@gmail.com>
2021-03-10 11:16:50 -0500

    Revert "Added EditScreen Issue IB2K#3"
    This reverts commit 3673a76ee879005b8951f12219f144e05863d317.

--- src/routes/ProfileStack.js
+++ src/routes/ProfileStack.js
@@ -4,7 +4,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import ProfileScreen from "../screens/ProfileScreen";
 import HomeScreen from "../screens/HomeScreen";
 import SignUpScreen from "../screens/SignUpScreen";
-import EditScreen from "../screens/EditProfileScreen";
 import colors from "../config/colors";
 
 const Stack = createStackNavigator();
@@ -27,14 +26,9 @@ const ProfileStack = () => {
         options={{ title: "Home" }}
       />
       <Stack.Screen
-        name="SignUpScreen"
+        name="Sign Up"
         component={SignUpScreen}
         options={{ title: "Sign Up" }}
-      />
-      <Stack.Screen
-        name="EditProfileScreen"
-        component={EditScreen}
-        options={{ title: "Edit Profile" }}
       />
     </Stack.Navigator>
   );
