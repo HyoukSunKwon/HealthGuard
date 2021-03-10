73f2ab7875d71e68f1be8c60056968f73fe82968
Elisa <elisangli11@gmail.com>
2021-03-10 11:14:58 -0500

    Revert "Updated screen and buttons Issue IB2K#3"
    This reverts commit 0c1e3e64f4787bb276077902f78da7c749f02c64.

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,48 +1,20 @@
 import React from "react";
-import { Image, StyleSheet, View, Text, SafeAreaView } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 
 import { AuthContext } from "../config/context";
 import screenConfig from "../config/screen";
 import colors from "../config/colors";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import DropDownPicker from "react-native-dropdown-picker";
 
 const ProfileScreen = ({ navigation }) => {
   const { signOut, signUp } = React.useContext(AuthContext);
-
-  let dayArr = [];
-
-  for (let i = 1; i <= 31; i++) {
-    dayArr.push({ label: i.toString(), value: i });
-  }
-
-  const monArr = [
-    { label: "January", value: "January" },
-    { label: "Febuary", value: "Febuary" },
-    { label: "March", value: "March" },
-    { label: "April", value: "April" },
-    { label: "May", value: "May" },
-    { label: "June", value: "June" },
-    { label: "July", value: "July" },
-    { label: "August", value: "August" },
-    { label: "September", value: "September" },
-    { label: "October", value: "October" },
-    { label: "November", value: "November" },
-    { label: "December", value: "December" },
-  ];
-
-  let yearArr = [];
-  let year = new Date().getFullYear();
-  for (let i = year; i >= 1920; i--) {
-    yearArr.push({ label: i.toString(), value: i });
-  }
-
-  const genderArr = [
-    { label: "Male", value: "Male" },
-    { label: "Female", value: "Female" },
-    { label: "Other", value: "Other" },
-  ];
-
   return (
     <SafeAreaView style={screenConfig.container}>
       <Image
@@ -50,60 +22,25 @@ const ProfileScreen = ({ navigation }) => {
         style={screenConfig.logoImage}
         source={require("../../assets/icon.png")}
       />
-      {/* <TouchableOpacity onPress={() => signUp()}>
+      <TouchableOpacity onPress={() => signUp()}>
         <Text style={styles.textCreate}> Wanna create an account? </Text>
-      </TouchableOpacity> */}
+      </TouchableOpacity>
 
       <View style={styles.buttonContainer}>
         <View style={styles.button}>
-          <TouchableOpacity
-            onPress={() =>
-              navigation.navigate("EditProfileScreen", { name: "Edit Profile" })
-            }
-          >
+          <TouchableOpacity onPress={() => console.log("Edit Press")}>
             <Text style={styles.buttonText}>Edit Profile</Text>
           </TouchableOpacity>
         </View>
       </View>
       <View style={styles.formContainer}>
         <View style={styles.fieldContainer}>
-          <Text style={styles.Datetext}>Date of Birth</Text>
-          <View style={styles.makeRow}>
-            <View style={styles.dayStyle}>
-              <Text style={styles.dateText}>Day</Text>
-              <DropDownPicker
-                items={dayArr}
-                defaultValue={1}
-                containerStyle={{ height: 40 }}
-              />
-            </View>
-            <View style={styles.monStyle}>
-              <Text style={styles.dateText}>Month</Text>
-              <DropDownPicker
-                items={monArr}
-                containerStyle={{ height: 40 }}
-                defaultValue="January"
-              />
-            </View>
-            <View style={styles.yearStyle}>
-              <Text style={styles.dateText}>Year</Text>
-              <DropDownPicker
-                items={yearArr}
-                containerStyle={{ height: 40 }}
-                defaultValue={year}
-              />
-            </View>
-          </View>
+          <Text style={styles.text}>Date of Birth</Text>
+          <TextInput style={styles.box} placeholder="Date of Birth" />
         </View>
         <View style={styles.fieldContainer}>
-          <Text style={styles.genderText}> Gender</Text>
-          <View style={styles.genderStyle}>
-            <DropDownPicker
-              items={genderArr}
-              containerStyle={{ height: 40 }}
-              defaultValue="Male"
-            />
-          </View>
+          <Text style={styles.text}> Gender</Text>
+          <TextInput style={styles.box} placeholder="Gender" />
         </View>
       </View>
       <View style={styles.buttonContainer}>
@@ -112,11 +49,11 @@ const ProfileScreen = ({ navigation }) => {
             <Text style={styles.buttonText}>Sign Out</Text>
           </TouchableOpacity>
         </View>
-        {/* <View style={styles.button}>
+        <View style={styles.button}>
           <TouchableOpacity onPress={() => console.log("Delete Press")}>
             <Text style={styles.buttonText}>Delete Account</Text>
           </TouchableOpacity>
-        </View> */}
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -147,42 +84,19 @@ const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
   },
-  Datetext: {
+  text: {
     fontSize: 18,
-    top: 55,
   },
   textCreate: {
     textAlign: "center",
     color: colors.darkBlue,
   },
   fieldContainer: {
-    marginLeft: "3%",
+    marginLeft: "10%",
   },
   formContainer: {
     flex: 1,
     bottom: 60,
-  },
-  makeRow: {
-    flexDirection: "row",
-    alignSelf: "center",
-    marginLeft: "18%",
-  },
-  dayStyle: { width: "18%" },
-  monStyle: { width: "30%", marginLeft: 10 },
-  yearStyle: { width: "23%", marginLeft: 10 },
-  dateText: {
-    fontSize: 15,
-    textAlign: "center",
-    marginBottom: 5,
-  },
-  genderStyle: {
-    width: "64%",
-    marginLeft: "27%",
-    bottom: 10,
-  },
-  genderText: {
-    fontSize: 18,
-    top: 22,
   },
 });
 
