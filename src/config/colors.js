fbff1a6cfdc69d156276a87e27dc30a0e8ebafa1
Hyunji Lee <hyunji.lee.778@gmail.com>
2021-04-29 14:47:48 -0400

    issue #76 Add and erase colours (#86)

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,14 +1,11 @@
 export default {
   white: "#fff",
-  grey: "#8b8989", // light grey
-  darkGrey: "#313131",
+  backgroundGrey: "#E5E5E5",
+  grey: "#D1D1D1", // light grey
   red: "#F54D4D", // red Space cherry
-  black: "#000",
   blue: "#2D80E2",
   blue2: "#81B5F1",
   blueBlack: "#1F263E",
   lightBlue: "#F3F7FC",
-  lightBlue_button: "#dDefff",
-  // lightBlue_button: "#e6f6fe",
-  darkBlue: "#2A6F97",
+  lightBlue2: "A9D0FE",
 };
