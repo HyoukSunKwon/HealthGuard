860aee5a1fb1bedf3767948e8e7b84f34c67e789
Hyunji Lee <hyunji.lee.778@gmail.com>
2021-05-10 17:11:27 -0400

    Add icons and colour configs (#97)
    * Add icons and colour configs
    
    * Add add-profile icon

--- src/config/colors.js
+++ src/config/colors.js
@@ -1,11 +1,10 @@
 export default {
   white: "#fff",
-  backgroundGrey: "#E5E5E5",
-  grey: "#D1D1D1", // light grey
-  red: "#F54D4D", // red Space cherry
+  backgroundGrey: "#F8F8F8",
+  grey: "#9C9C9C",
+  red: "#F54D4D",
   blue: "#2D80E2",
   blue2: "#81B5F1",
-  blueBlack: "#1F263E",
-  lightBlue: "#F3F7FC",
-  lightBlue2: "#A9D0FE",
+  blue3: "#F3F7FC",
+  black: "#1F263E",
 };
