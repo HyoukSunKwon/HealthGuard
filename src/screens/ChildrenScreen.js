8e09d6537b62a316f4a1f0b67d1f09b6b1c1c378
Skylar Kwon <ddauppo@gmail.com>
2021-05-28 17:30:35 -0400

    issue#105 - apply scale, resizemode, navigation //  adjust start line and height

--- src/screens/ChildrenScreen.js
+++ src/screens/ChildrenScreen.js
@@ -1,158 +1,145 @@
 import React from "react";
-import {
-  ScrollView,
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Text,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, View, Text, SafeAreaView } from "react-native";
 
+import { scale, moderateScale, verticalScale } from "../config/scaling";
 import colors from "../config/colors";
-import screen from "../config/screen";
+// import font from "../config/context";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
+const users = [
+  {
+    id: 1,
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+  {
+    id: 2,
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+  {
+    id: 3,
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+];
 
 const ChildrenScreen = ({ navigation }) => {
-  const historyArr = [
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note:
-        "Next does in date: 30-Mar-2021 dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-  ];
-
-  renderHistories = () => {
-    return (
-      <View>
-        {historyArr.map((data, index) => {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.header}>
+        {users.map((u) => {
           return (
-            <View style={styles.container}>
-              <View style={styles.listStyle}>
-                <TouchableOpacity
-                  onPress={() =>
-                    navigation.navigate("ScheduleScreen", {
-                      name: "ScheduleScreen",
-                    })
-                  }
-                >
-                  <Text style={styles.type}>{data.type}</Text>
-                  <Text style={styles.date}>
-                    <Ionicons
-                      name="calendar"
-                      size={15}
-                      color={colors.darkGrey}
-                    ></Ionicons>
-                    {data.day} {data.month}, {data.year}
-                  </Text>
-                  <Text numberOfLines={1} style={styles.note}>
-                    {data.note}
-                  </Text>
-                </TouchableOpacity>
-                <Ionicons
-                  name="ios-arrow-forward"
-                  size={30}
-                  color={colors.lightBlue}
-                  style={styles.arrowIcon}
-                ></Ionicons>
-              </View>
+            <View key={u.id}>
+              <TouchableOpacity
+                onPress={() => navigation.navigate("ScheduleScreen")}
+              >
+                <View style={styles.container}>
+                  {/* <Image source={{ uri:u.avatar }} /> */}
+                  <Image
+                    style={styles.image}
+                    source={require("../../assets/icons/logo.png")}
+                  />
+                  <View style={styles.body}>
+                    <Text style={styles.name}>{u.name}</Text>
+                    <View style={styles.lineContainer}>
+                      <Image
+                        style={styles.icon}
+                        source={require("../../assets/icons/calendar.png")}
+                      />
+                      <Text style={styles.birth}>BIRTH: {u.birth}</Text>
+                    </View>
+                    <View style={styles.lineContainer}>
+                      <Image
+                        style={styles.icon}
+                        source={require("../../assets/icons/injection.png")}
+                      />
+                      <Text style={styles.birth}>LATEST: {u.latest}</Text>
+                    </View>
+                  </View>
+                </View>
+              </TouchableOpacity>
             </View>
           );
         })}
       </View>
-    );
-  };
-  return (
-    <View style={screen.container}>
-      <ScrollView>{renderHistories()}</ScrollView>
-      <Ionicons
-        name="ios-add-circle"
-        size={90}
-        color={colors.lightBlue}
-        style={styles.addIcon}
-        onPress={() =>
-          navigation.navigate("AddChildScreen", { screen: "AddChildScreen" })
-        }
-      ></Ionicons>
+      <View style={styles.foot}>
+        <TouchableOpacity onPress={() => navigation.navigate("AddChildScreen")}>
+          <Image
+            style={styles.addIcon}
+            source={require("../../assets/icons/add-profile.png")}
+          ></Image>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: "95%",
-    marginLeft: "5%",
-    marginTop: "2%",
-    paddingBottom: "2%",
-    borderBottomColor: colors.lightBlue_button,
-    borderBottomWidth: 1,
-  },
-  listStyle: { paddingRight: "12%" },
-  type: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: colors.lightBlue,
-  },
-  date: {
-    fontSize: 15,
-  },
-  note: { fontSize: 14 },
-  arrowIcon: {
+  mainContainer: {
     flex: 1,
-    alignItems: "center",
-    position: "absolute",
-    marginTop: "6%",
-    right: 0,
+  },
+  header: {
+    flex: 0.8,
+  },
+  foot: {
+    flex: 0.2,
+  },
+  container: {
+    height: moderateScale(96),
+    backgroundColor: colors.white,
+    flexDirection: "row",
+    marginBottom: scale(10),
+  },
+  lineContainer: {
+    flexDirection: "row",
+  },
+  name: {
+    fontFamily: "notoSans-bold",
+    fontSize: moderateScale(16),
+    lineHeight: moderateScale(22),
+    margin: scale(5),
+    color: colors.black,
+  },
+  birth: {
+    fontFamily: "roboto-regular",
+    fontSize: moderateScale(12),
+    color: colors.black,
+    lineHeight: verticalScale(16.5),
+    margin: scale(5),
+  },
+  image: {
+    flex: 1,
+    resizeMode: "center",
+    height: verticalScale(66),
+    width: scale(66),
+    margin: scale(10),
+    marginTop: verticalScale(15),
+    justifyContent: "space-around",
+    borderRadius: scale(400 / 2),
+  },
+  body: {
+    flex: 2,
+    justifyContent: "center",
   },
   addIcon: {
-    flex: 1,
-    position: "absolute",
     alignSelf: "flex-end",
-    bottom: 0,
-    right: 0,
+    width: scale(70),
+    height: verticalScale(70),
+    marginRight: moderateScale(23),
+  },
+  icon: {
+    marginTop: verticalScale(8),
+    marginLeft: scale(4),
+    height: verticalScale(10),
+    width: scale(10),
+    resizeMode: "stretch",
   },
 });
 
--- src/screens/EditChildrenScreen.js
+++ src/screens/EditChildrenScreen.js
@@ -1,157 +1,171 @@
 import React from "react";
-import {
-  ScrollView,
-  TouchableOpacity,
-  StyleSheet,
-  View,
-  Text,
-} from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { Image, StyleSheet, View, Text, SafeAreaView } from "react-native";
 
+import { scale, moderateScale, verticalScale } from "../config/scaling";
 import colors from "../config/colors";
+import { TouchableOpacity } from "react-native-gesture-handler";
 import screen from "../config/screen";
+// import font from "../config/context";
+
+const users = [
+  {
+    id: 1,
+    image: "../../assets/icons/logo.png",
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+  {
+    id: 2,
+    image: "../../assets/icons/logo.png",
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+  {
+    id: 3,
+    image: "../../assets/icons/logo.png",
+    name: "JasperMattews",
+    birth: "Jan 15th 2020",
+    latest: "2020-03-20",
+    avatar: "https://source.unsplash.com/user/nicoleknipes",
+  },
+];
 
 const EditChildrenScreen = ({ navigation }) => {
-  const historyArr = [
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021 dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-    {
-      type: "Covid-19",
-      day: 1,
-      month: "January",
-      year: 2021,
-      note: "Next does in date: 30-Mar-2021",
-    },
-  ];
-
-  renderHistories = () => {
-    return (
-      <View>
-        {historyArr.map((data, index) => {
+  return (
+    <View style={styles.mainContainer}>
+      <View style={styles.header}>
+        {users.map((u) => {
           return (
-            <View style={styles.container}>
-              <View style={styles.listStyle}>
-                <TouchableOpacity
-                  onPress={() =>
-                    navigation.navigate("ScheduleScreen", {
-                      name: "ScheduleScreen",
-                    })
-                  }
-                >
-                  <Text style={styles.type}>{data.type}</Text>
-                  <Text style={styles.date}>
-                    <Ionicons
-                      name="calendar"
-                      size={15}
-                      color={colors.darkGrey}
-                    ></Ionicons>
-                    {data.day} {data.month}, {data.year}
-                  </Text>
-                  <Text numberOfLines={1} style={styles.note}>
-                    {data.note}
-                  </Text>
-                </TouchableOpacity>
-                <Ionicons
-                  name="ios-arrow-forward"
-                  size={30}
-                  color={colors.lightBlue}
-                  style={styles.arrowIcon}
-                ></Ionicons>
+            <View key={u.id}>
+              <View style={styles.container}>
+                {/* <Image source={{ uri:u.avatar }} /> */}
+                <Image
+                  style={styles.image}
+                  source={require("../../assets/icons/logo.png")}
+                />
+                <View style={styles.body}>
+                  <Text style={styles.name}>{u.name}</Text>
+                  <View style={styles.lineContainer}>
+                    <Image
+                      style={styles.lineIcon}
+                      source={require("../../assets/icons/calendar.png")}
+                    />
+                    <Text style={styles.birth}>BIRTH: {u.birth}</Text>
+                  </View>
+                  <View style={styles.lineContainer}>
+                    <Image
+                      style={styles.lineIcon}
+                      source={require("../../assets/icons/injection.png")}
+                    />
+                    <Text style={styles.birth}>LATEST: {u.latest}</Text>
+                  </View>
+                </View>
+                <View style={styles.smallIcon}>
+                  <Image
+                    style={styles.icon}
+                    source={require("../../assets/icons/delete.png")}
+                  />
+                  <TouchableOpacity
+                    onPress={() => navigation.navigate("EditProfileScreen")}
+                  >
+                    <Image
+                      style={styles.icon}
+                      source={require("../../assets/icons/edit.png")}
+                    />
+                  </TouchableOpacity>
+                </View>
               </View>
             </View>
           );
         })}
       </View>
-    );
-  };
-  return (
-    <View style={screen.container}>
-      <ScrollView>{renderHistories()}</ScrollView>
-      <Ionicons
-        name="ios-add-circle"
-        size={90}
-        color={colors.lightBlue}
-        style={styles.addIcon}
-        onPress={() =>
-          navigation.navigate("ChildrenScreen", { screen: "ChildrenScreen" })
-        }
-      ></Ionicons>
+      <View style={styles.foot}>
+        <TouchableOpacity
+          style={screen.buttonContainer}
+          onPress={() =>
+            navigation.navigate("ChildrenScreen", { name: "Children" })
+          }
+        >
+          <Text style={screen.buttonText}> DONE</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+  },
+  header: {
+    flex: 0.8,
+  },
+  foot: {
+    flex: 0.2,
+  },
   container: {
-    width: "95%",
-    marginLeft: "5%",
-    marginTop: "2%",
-    paddingBottom: "2%",
-    borderBottomColor: colors.lightBlue_button,
-    borderBottomWidth: 1,
+    height: verticalScale(96),
+    backgroundColor: colors.white,
+    flexDirection: "row",
+    marginBottom: verticalScale(10),
   },
-  listStyle: { paddingRight: "12%" },
-  type: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: colors.lightBlue,
+  name: {
+    fontFamily: "roboto-bold",
+    fontSize: moderateScale(16),
+    lineHeight: verticalScale(22),
+    margin: scale(5),
   },
-  date: {
-    fontSize: 15,
+  lineContainer: {
+    flexDirection: "row",
   },
-  note: { fontSize: 14 },
-  arrowIcon: {
+  birth: {
+    fontFamily: "roboto-regular",
+    fontSize: moderateScale(12),
+    color: colors.black,
+    lineHeight: verticalScale(16.5),
+    margin: scale(5),
+  },
+  image: {
     flex: 1,
-    alignItems: "center",
-    position: "absolute",
-    marginTop: "6%",
-    right: 0,
+    resizeMode: "center",
+    height: verticalScale(66),
+    width: scale(66),
+    margin: scale(10),
+    marginTop: verticalScale(15),
+    justifyContent: "space-around",
+    borderRadius: scale(400 / 2),
   },
-  addIcon: {
-    flex: 1,
-    position: "absolute",
-    alignSelf: "flex-end",
-    bottom: 0,
-    right: 0,
+  body: {
+    flex: 2,
+    justifyContent: "center",
+  },
+  icon: {
+    height: moderateScale(12),
+    width: scale(12),
+    justifyContent: "center",
+    marginTop: verticalScale(25),
+    marginBottom: verticalScale(2),
+    marginLeft: scale(20),
+    resizeMode: "stretch",
+  },
+  smallIcon: {
+    flexDirection: "column",
+    flex: 0.6,
+  },
+  dataContainer: {
+    marginTop: verticalScale(60),
+    textAlign: "center",
+  },
+  lineIcon: {
+    marginTop: verticalScale(8),
+    marginLeft: scale(4),
+    height: verticalScale(10),
+    width: scale(10),
+    resizeMode: "stretch",
   },
 });
 
